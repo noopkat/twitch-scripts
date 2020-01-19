ddc1a1c6db0a5a76cd1a40a71239098bdc631d67
Suz Hinton <suz.hinton@gmail.com>
2020-01-19 14:21:08 -0800

    add tcp socket and chat handler for vim line signs

--- twitch-mod-tools/modtooldaemon.js
+++ twitch-mod-tools/modtooldaemon.js
@@ -1,6 +1,11 @@
 const tmi = require('tmi.js');
 const robot = require('robotjs');
 const notifier = require('node-notifier');
+const net = require('net');
+
+const tcpHost = 'localhost';
+const tcpPort = 6969;
+let tcpsock;
 
 const channelname = 'noopkat';
 
@@ -14,7 +19,29 @@ function handleMessage(channel, tags, message) {
 
   if (message.toLowerCase().substring(0,6) === '!scene' && mod) switchScene(message);
   if (message.includes('STOP')) sendStopNotification(tags.username);
+  if (message.toLowerCase().substring(0,5) === '!line') placeVimSign(message, tags);
 };
+
+function handleTcpConnection(sock) {
+ // only vim is using this so just replace the socket used every time
+ tcpsock = sock;
+ console.log(`vim is connected: ${sock.remoteAddress}: ${sock.remotePort}`);
+
+ sock.on('close', function(data) {
+   console.log(`vim closed connection: ${sock.remoteAddress} ${sock.remotePort}`);
+ });
+}
+
+function placeVimSign(message, tags) {
+  const nick = "@" + tags.username;
+  const line = parseInt(message.split(' ')[1], 10);
+  const suggestion = message.split(' ').slice(2).join(' ');
+
+  const payload = [0, { line, nick, suggestion }];
+  const jsonPayload = JSON.stringify(payload);
+
+  tcpsock.write(jsonPayload + '\n');
+}
 
 function switchScene(message) {
   const scene = message.toLowerCase().split(' ')[1];
@@ -31,7 +58,7 @@ function sendStopNotification(username) {
   });
 }
 
-const client = new tmi.Client({
+const ircClient = new tmi.Client({
   options: {debug: true},
   connection: {
     reconnect: true,
@@ -40,6 +67,12 @@ const client = new tmi.Client({
   channels: [channelname]
 });
 
-client.connect();
-client.on('message', handleMessage);
+ircClient.connect();
+ircClient.on('message', handleMessage);
+
+// tcp server for vim channel
+const tcpServer = net.createServer()
+tcpServer.on('connection', handleTcpConnection);
+tcpServer.listen(tcpPort, tcpHost);
+console.log('Server listening on ' + tcpHost +':'+ tcpPort);
 
